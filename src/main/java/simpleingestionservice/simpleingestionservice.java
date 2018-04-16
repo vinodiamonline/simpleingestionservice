a6ba8d37fd7dae617072b27d7d064842a17acd23
vinkumar <vinkumar@adobe.com>
2018-04-16 22:03:50 +0530

    added ingestion related call

--- src/main/java/simpleingestionservice/simpleingestionservice.java
+++ src/main/java/simpleingestionservice/simpleingestionservice.java
@@ -1,10 +1,16 @@
 package simpleingestionservice;
 
+import simpleingestion.GetWeatherData;
+//
+
 public class simpleingestionservice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		GetWeatherData getdata = new GetWeatherData();
 		
+		getdata.ingest();
 	}
 
 }
+
